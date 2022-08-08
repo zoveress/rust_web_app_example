8c0b2977fec6cadac7aa851ddeaa6257b601e55e
root <zoveress@protonmail.com>
2022-08-08 09:34:42 +0200

    Adding counting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-use rand::Rng;
+use std::time::Duration;
+use std::thread::sleep;
+let mut count = 0u32;
 
 fn main() {
+    println!("Let's count until infinity!");
 
-    let mut rng = rand::thread_rng();
-    println!("Your lucky number today is : {}", rng.gen_range(0, 10));
+    // Infinite loop
+    loop {
+        count += 1;
+        println!("Counting: ", count);
+        sleep(Duration::from_millis(5));
+
+    }
 }
